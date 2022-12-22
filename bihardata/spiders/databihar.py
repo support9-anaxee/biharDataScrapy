36c2d8368b8d999a6e5c3f5a122d0288880f705d
devrahul <rbhadoria97@gmail.com>
2022-12-22 16:53:57 +0530

    addin csv

--- bihardata/spiders/databihar.py
+++ bihardata/spiders/databihar.py
@@ -1,3 +1,4 @@
+
 import scrapy
 import pandas as pd
 from scrapy.utils.response import open_in_browser
@@ -6,59 +7,64 @@ from scrapy import FormRequest, Request
 
 class VillageData(scrapy.Spider):
     name = 'ffdata'
-
-    def start_requests(self):
-        urls = [
-            'http://epds.bihar.gov.in/RCList_Village_Wise.aspx?Village_Code_PMO=1020301019216791&&Village_Name_PMO=Bassganw&&Panchayat_Code_PMO=001162&&Panchayat_Name_PMO=BANSGAWNMANJHARIYA&&District_Code_PMO=203&&District_Name_PMO=PashchimChamparan&&Tahsil_Code_PMO=01019&&Tahsil_Name_PMO=Bagaha'
-        ]
-        for url in urls:
-            yield scrapy.Request(url=url, callback=self.parse)
-
+    start_urls = ['http://epds.bihar.gov.in/RCList_Village_Wise.aspx?Village_Code_PMO=1020301019216791&&Village_Name_PMO=Bassganw&&Panchayat_Code_PMO=001162&&Panchayat_Name_PMO=BANSGAWNMANJHARIYA&&District_Code_PMO=203&&District_Name_PMO=PashchimChamparan&&Tahsil_Code_PMO=01019&&Tahsil_Name_PMO=Bagaha']
 
     def parse(self, response):
-        # extract data from 
+        pages = response.css('table')[2].css('tr').css('td').css('a::attr(href)').extract()
+        tex = response.css('table')[2].css('tr').css('td').css('font ::text').extract()[10]
+      
         rows= response.css('table.cellstyle tr')
-        url = 'http://epds.bihar.gov.in/RCList_Village_Wise.aspx?Village_Code_PMO=1020301019216791&&Village_Name_PMO=Bassganw&&Panchayat_Code_PMO=001162&&Panchayat_Name_PMO=BANSGAWNMANJHARIYA&&District_Code_PMO=203&&District_Name_PMO=PashchimChamparan&&Tahsil_Code_PMO=01019&&Tahsil_Name_PMO=Bagaha'    
-        pages = response.css('table')[2].css('tr').css('td').css('a::attr(href)').getall()
-        for page in pages: 
-            print
-            yield Request(url, formdata={'__EVENTTARGET': 'gridmain','__EVENTARGUMENT':'Page$2'}, callback=self.step2)
-            return
-            # print(d)
-        # for page in pages:
-        #     if page is not None:
-        #         yield response.follow(url+page, self.step2)
-
-        #     url = 'http://epds.bihar.gov.in/RCList_Village_Wise.aspx?Village_Code_PMO=1020301019216791&&Village_Name_PMO=Bassganw&&Panchayat_Code_PMO=001162&&Panchayat_Name_PMO=BANSGAWNMANJHARIYA&&District_Code_PMO=203&&District_Name_PMO=PashchimChamparan&&Tahsil_Code_PMO=01019&&Tahsil_Name_PMO=Bagaha'
-        #     data = {
-        #     '__EVENTTARGET': 'gridmain',
-        #     '__EVENTARGUMENT': 'Page$'+str(page)
-        #         }    
-        #     yield FormRequest(url,formdata=data, callback=self.step2)
-        #     return
-            # print("@@@", data)
-
+        for row in rows[1:51]:
+            # data = row.css('td').extract()
+            yield{
+                "SNO": row.css('td')[0].css('span::text').extract()[0],
+                "Ration_Card" : "'"+row.css('td')[1].css('a').css('font::text').extract()[0]+"'",
+                "Card_Type" : row.css('td')[2].css('font::text').extract()[0].split('\r\n ')[1].replace(" ", ""),
+                "Ratio_Card_Holder_Name" : row.css('td')[3].css('font::text').extract()[0].split('\r\n ')[1].replace(" ", ""),
+                "Father_Name" : row.css('td')[4].css('font::text').extract()[0].split('\r\n ')[1].replace(" ", ""),
+                "Number_of_Family_Members" : row.css('td')[5].css('font::text').extract()[0].split('\r\n ')[1].replace(" ", ""),
+                "FPS Dealer" :  row.css('td')[6].css('font::text').extract()[0].split('\r\n ')[1].replace(" ", "") +row.css('td')[6].css('font::text').extract()[1].replace("\r\n", "").replace(" ",""),
+                "State" : response.css('table tr').css('td')[1].css('b::text').extract()[0],
+                "District": response.css('table tr').css('td')[2].css('b').css('span ::text').extract()[0],
+                "Block" : response.css('table tr').css('td')[3].css('b').css('span ::text').extract()[0],
+                "Panchayat" :  response.css('table tr').css('td')[4].css('b').css('span ::text').extract()[0],
+                "Village":response.css('table tr').css('td')[5].css('b').css('span ::text').extract()[0],
+            }
+        # for row in range(1,24):    
+        for page in pages:
+            row= page.split('$')[1].replace("')","")
+            # print(row)
+            data = {
+            '__EVENTTARGET': 'gridmain',
+            '__EVENTARGUMENT': 'Page$'+row      
+            }
+            # print(data)
+            yield FormRequest.from_response(response,formdata=data, callback=self.step2)   
+        
+ 
     def step2(self, response):
-        print("@@@@",response.body)
+        # extract data from 
+        # pages = response.css('table')[2].css('tr').css('td').css('a::attr(href)').extract()
+        # for page in pages[1:]:
+        #     row= page.split('$')[1].replace("')","")
+        # print(pages)
+        rows= response.css('table.cellstyle tr')
+        for row in rows[1:51]:
+            yield{
+                "SNO": row.css('td')[0].css('span::text').extract()[0],
+                "Ration_Card" : "'"+row.css('td')[1].css('a').css('font::text').extract()[0]+"'",
+                "Card_Type" : row.css('td')[2].css('font::text').extract()[0].split('\r\n ')[1].replace(" ", ""),
+                "Ratio_Card_Holder_Name" : row.css('td')[3].css('font::text').extract()[0].split('\r\n ')[1].replace(" ", ""),
+                "Father_Name" : row.css('td')[4].css('font::text').extract()[0].split('\r\n ')[1].replace(" ", ""),
+                "Number_of_Family_Members" : row.css('td')[5].css('font::text').extract()[0].split('\r\n ')[1].replace(" ", ""),
+                "FPS Dealer" :  row.css('td')[6].css('font::text').extract()[0].split('\r\n ')[1].replace(" ", "") +row.css('td')[6].css('font::text').extract()[1].replace("\r\n", "").replace(" ",""),
+                "State" : response.css('table tr').css('td')[1].css('b::text').extract()[0],
+                "District": response.css('table tr').css('td')[2].css('b').css('span ::text').extract()[0],
+                "Block" : response.css('table tr').css('td')[3].css('b').css('span ::text').extract()[0],
+                "Panchayat" :  response.css('table tr').css('td')[4].css('b').css('span ::text').extract()[0],
+                "Village":response.css('table tr').css('td')[5].css('b').css('span ::text').extract()[0],
+            }
             
-        # print(response.css('td').extract())
-        # for row in rows[1:51]:
-        #     data = row.css('td').extract()
-        #     name = {
-        #         "SNO": row.css('td')[0].css('span::text').extract()[0],
-        #         "Ration_Card" : row.css('td')[1].css('a').css('font::text').extract()[0],
-        #         "Card_Type" : row.css('td')[2].css('font::text').extract()[0].split('\r\n ')[1].replace(" ", ""),
-        #         "Ratio_Card_Holder_Name" : row.css('td')[3].css('font::text').extract()[0].split('\r\n ')[1].replace(" ", ""),
-        #         "Father_Name" : row.css('td')[4].css('font::text').extract()[0].split('\r\n ')[1].replace(" ", ""),
-        #         "Number_of_Family_Members" : row.css('td')[5].css('font::text').extract()[0].split('\r\n ')[1].replace(" ", ""),
-        #         "FPS Dealer" :  row.css('td')[6].css('font::text').extract()[0].split('\r\n ')[1].replace(" ", "") +row.css('td')[6].css('font::text').extract()[1].replace("\r\n", "").replace(" ",""),
-        #         "State" : response.css('table tr').css('td')[1].css('b::text').extract()[0],
-        #         "District": response.css('table tr').css('td')[2].css('b').css('span ::text').extract()[0],
-        #         "Block" : response.css('table tr').css('td')[3].css('b').css('span ::text').extract()[0],
-        #         "Panchayat" :  response.css('table tr').css('td')[4].css('b').css('span ::text').extract()[0],
-        #         "Village":response.css('table tr').css('td')[5].css('b').css('span ::text').extract()[0],
-        #     }
-        #     return name
         
 
 
